57f1e47597620496b03a15bc8ffa3e38b0286a6f
Pranay Desai <pdesai22@stevens.edu>
2023-06-27 12:34:46 -0400

    change server.js to connect mongo cluster

--- server.js
+++ server.js
@@ -5,20 +5,23 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 const mongoose = require('mongoose');
 let DB;
+
+DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD_OVS
+);
+
 //CONNECT DATABASE ON_LINE or OFFLINE
-process.argv.forEach((argv) => {
-  if (argv === '--online') {
-    DB = process.env.DATABASE.replace(
-      '<password>',
-      process.env.DATABASE_PASSWORD_OVS
-    );
-  } else {
-    DB = process.env.DATABASE_LOCAL.replace(
-      '<password>',
-      process.env.DATABASE_PASSWORD_OVS
-    );
-  }
-});
+// process.argv.forEach((argv) => {
+//   // if (argv === '--online') {
+
+//   // } else {
+//   //   DB = process.env.DATABASE_LOCAL.replace(
+//   //     '<password>',
+//   //     process.env.DATABASE_PASSWORD_OVS
+//   //   );
+//   // }
+// });
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
